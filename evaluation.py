141115bf8a808c9e8345bf22c2183fc98fef2efb
Maximilian <maximilian.muschalik@gmail.com>
2023-01-11 20:53:12 +0100

    wrote plotting function that takes experiment df as input

--- evaluation.py
+++ evaluation.py
@@ -1,91 +1,108 @@
 import pandas as pd
-import seaborn as sns
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-if __name__ == "__main__":
-    sns.set_theme(style="darkgrid")
+#  COLORS = {'SII': '#058ED9', 'STI': '#2D3142', 'SFI': '#CC2D35'}  # online inclusive colors
+COLORS = {'SII': '#44cfcb', 'STI': '#7d53de', 'SFI': '#ef27a6'}
+MARKERS = {'SII': 'X', 'STI': 'o', 'SFI': "s"}
+LABELS = {'SII': 'Shapley Interaction', 'STI': 'Shapley Taylor', 'SFI': "Shapley Faith"}
 
-    #file_name = "results/sparse_linear_model_10.csv"
-    file_name = "sparse_linear_model_10_False.csv"
-    #file_name = "interaction_sampling_k.csv"
+STD_ALPHA = 0.10
 
-    data = pd.read_csv(file_name)
 
-    shapley_interaction_order = str(data["shapley_interaction_order"][0])
-    data = data[data['full'].isin(['not full', np.NAN])].drop(columns=['full'])
-    data = data[data['sampling_kernel'].isin(['faith', np.NAN])]
-    #data = data.drop(columns=['pairing', 'sampling_kernel', 'shapley_interaction_order'])
+def draw_approx_curve(df: pd.DataFrame, figsize: tuple = (10, 10),
+                      x_min: int = None, y_max: float = None, y_min: float = None, shading: str = None, plot_title: str = None,
+                      x_label: str = None, y_label: str = None):
+
+    grouping = ['n_absolute']
+
+    data = df.copy()
+    data = data[data['sampling'].isin(['const and sampling', np.NAN])].drop(columns=['sampling'])
     data = data[data['n_absolute'] > 0]
 
     data = data.rename(columns={"interaction_index": "Interaction Index", "approx_type": "Method"})
 
-    plot_title = ' '.join(("Order:", shapley_interaction_order))
+    fig, axis = plt.subplots(1, 1, figsize=figsize)
 
-    ax = sns.lineplot(
-        x='n_absolute', y="approx_value",
-        hue="Interaction Index", style='Method',
-        data=data
-    )
-    ax.set(ylabel='Approximation Error', xlabel='N Samples (absolute)',
-           title=plot_title,
-           #ylim=(-1, 5)
-    )
+    for interaction_index in data["Interaction Index"].unique():
+        subset = data[data["Interaction Index"] == interaction_index]
+        if x_min is not None:
+            subset = subset[subset["n_absolute"] >= x_min]
+
+        if 'inner_iteration' in subset.columns:
+            subset = subset.groupby(by=['iteration', 'n_absolute', "Method"]).aggregate({"approx_value": "mean"}).reset_index()
+
+        baseline = subset[subset["Method"] == "baseline"]
+        approximation = subset[subset["Method"] == "approximation"]
+
+        baseline_mean = baseline.groupby(by=grouping)["approx_value"].quantile(q=0.5).reset_index()
+        baseline_quantile_l = baseline.groupby(by=grouping)["approx_value"].quantile(q=0.25).reset_index()["approx_value"]
+        baseline_quantile_h = baseline.groupby(by=grouping)["approx_value"].quantile(q=0.75).reset_index()["approx_value"]
+        approximation_mean = approximation.groupby(by=grouping)["approx_value"].quantile(q=0.5).reset_index()
+        approximation_quantile_l = approximation.groupby(by=grouping)["approx_value"].quantile(q=0.25).reset_index()["approx_value"]
+        approximation_quantile_h = approximation.groupby(by=grouping)["approx_value"].quantile(q=0.75).reset_index()["approx_value"]
+
+        if shading == 'std':
+            approximation_std = approximation.groupby(by=grouping)["approx_value"].std().reset_index()
+            approximation_quantile_l = approximation_mean["approx_value"] - approximation_std["approx_value"]
+            approximation_quantile_h = approximation_mean["approx_value"] + approximation_std["approx_value"]
+            baseline_std = baseline.groupby(by=grouping)["approx_value"].std().reset_index()
+            baseline_quantile_l = baseline_mean["approx_value"] - baseline_std["approx_value"]
+            baseline_quantile_h = baseline_mean["approx_value"] + baseline_std["approx_value"]
+
+        axis.plot(baseline_mean["n_absolute"], baseline_mean["approx_value"],
+                  ls="dashed", color=COLORS[interaction_index], linewidth=1,
+                  marker=MARKERS[interaction_index], mec="white")
+        axis.plot(approximation_mean["n_absolute"], approximation_mean["approx_value"],
+                  ls="solid", color=COLORS[interaction_index], linewidth=1,
+                  marker=MARKERS[interaction_index], mec="white")
+
+        if shading is not None:
+            try:
+                axis.fill_between(approximation_mean["n_absolute"],
+                                  approximation_quantile_l,
+                                  approximation_quantile_h,
+                                  color=COLORS[interaction_index], alpha=STD_ALPHA, linewidth=0.)
+            except Exception as e:
+                print("Error occurred in baseline std:", e)
+            try:
+                axis.fill_between(approximation_mean["n_absolute"],
+                                  baseline_quantile_l,
+                                  baseline_quantile_h,
+                                  color=COLORS[interaction_index], alpha=STD_ALPHA, linewidth=0.)
+            except Exception as e:
+                print("Error occurred in approximation std:", e)
+
+    title_1 = "$\\bf{Indices}$"
+    axis.plot([], [], label=title_1, color="none")
+    for interaction_index in data["Interaction Index"].unique():
+        axis.plot([], [], color=COLORS[interaction_index],
+                  marker=MARKERS[interaction_index], label=LABELS[interaction_index], mec="white")
+
+    title_2 = "$\\bf{Methods}$"
+    axis.plot([], [], label=title_2, color="none")
+    axis.plot([], [], color="black", ls="solid", label="SHAPX")
+    axis.plot([], [], color="black", ls="dashed", label="baseline")
+
+    leg = axis.legend()
+    for item, label in zip(leg.legendHandles, leg.texts):
+        if label._text in [title_1, title_2]:
+            width = item.get_window_extent(fig.canvas.get_renderer()).width
+            label.set_ha('left')
+            label.set_position((-2 * width, 0))
+
+    plt.title(plot_title)
+    axis.set_xlabel(x_label)
+    axis.set_ylabel(y_label)
+    axis.set_ylim((y_min, y_max))
+    plt.tight_layout()
     plt.show()
 
 
-    """
-    # approximation only
-    #data = data[data["approx_type"].isin(['approximation',np.NAN])]
-    #subset sizes
-    #data = data[data["n_absolute"]>2000]
-    #select single index
-    #data = data[data["interaction_index"].isin(["SII",np.NAN])]
-    #filter only approximation
-    #data = data[data["approx_type"].isin(["approximation"])]
-    #data = data.drop(columns=['sampling_kernel'])
-    
-    data_grouped = data.groupby(
-        by=['iteration', 'approx_type', 'interaction_index', 'n_absolute', 'sampling_kernel', 'pairing', 'full'],
-        dropna=False).agg({'approx_value': ['mean', 'std'], 'n_relative': 'first'}).reset_index()
-    for interaction_index in data_grouped.interaction_index.unique():
-        data_df = data_grouped[data_grouped.interaction_index == interaction_index]
-    
-
-    interaction_types = data["interaction_index"].unique()
-
-    #Approximation Comparison
-    for interaction_type in interaction_types:
-        sampling_kernels = data["sampling_kernel"].dropna().unique()
-        for kernel in sampling_kernels:
-            #Select Kernel
-            data_plot = data[data['sampling_kernel'].isin([kernel,np.NAN])]
-            data_plot = data_plot[data_plot['interaction_index'].isin([interaction_type,np.NAN])]
-            data_plot = data_plot[data_plot['full'].isin(['not full', np.NAN])].drop(columns=['full'])
-            plt.figure()
-            ax = sns.lineplot(
-                x='n_absolute', y="approx_value",
-                hue="approx_type", style='interaction_index',
-                data=data_plot
-            )
-            ax.set(ylabel='Approx Error', xlabel='N Samples (relative)', title="Index: "+interaction_type+" Order: "+str(shapley_interaction_order) + " kernel:"+kernel)
-            plt.show()
-
-    #Visualization Kernel Comparison
-    for interaction_type in interaction_types:
-        data_plot = data[data["approx_type"].isin(['approximation',np.NAN])]
-        #Select Kernel
-        data_plot = data_plot[data_plot['interaction_index'].isin([interaction_type,np.NAN])]
-        data_plot = data_plot[data_plot['full'].isin(['not full', np.NAN])].drop(columns=['full'])
-        data_plot = data_plot[data_plot['sampling_kernel'].isin(['unif-size', np.NAN])]
-
-        plt.figure()
-        ax = sns.lineplot(
-            x='n_absolute', y="approx_value",
-            hue="sampling_kernel", style='interaction_index',
-            data=data_plot
-        )
-        ax.set(ylabel='Approx Error', xlabel='N Samples (relative)', title="Index: "+interaction_type+" Order: "+str(shapley_interaction_order) + " kernel:"+kernel)
-        plt.show()
-        """
-
+if __name__ == "__main__":
+    file_name = "results/synth_neural_network_11.csv"
+    plot_title = r"Synthetic Neural Network ($l = 2$, $n = 14$, $g = 20$)"
+    draw_approx_curve(df=pd.read_csv(file_name), figsize=(6, 5), x_min=250, shading="quant",
+                      plot_title=plot_title,
+                      y_label="average squared distance", x_label="model evaluations")
